f9541d4adc120d66c0a7fd1673e0cdb0348791f4
8c6t <nkw8620@gmail.com>
2019-09-12 21:25:55 +0900

    6.4 테스트가 실패했을 때 상태 기록하기

--- ch06/makrdown_editor/src/renderer/Components/Previewer.jsx
+++ ch06/makrdown_editor/src/renderer/Components/Previewer.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import marked from 'marked';
 import classnames from 'classnames/bind';
+import emojione from 'emojione';
 
 import style from './Previewer.module.css';
 
 const cx = classnames.bind(style);
 
 marked.setOptions({ sanitize: true });
+
+const renderer= new marked.Renderer();
+renderer.text = (text) => {
+  return emojione.shortnameToImage(text);
+}
 
 const Previewer = ({ className, value }) => {
   return (
@@ -15,7 +21,7 @@ const Previewer = ({ className, value }) => {
       className={cx(className, 'previewer')}
     >
     <span
-      dangerouslySetInnerHTML={{ __html: marked(value) }}
+      dangerouslySetInnerHTML={{ __html: marked(value, { renderer }) }}
     />
     </div>
   )
--- ch06/makrdown_editor/test/inputEditor_test.js
+++ ch06/makrdown_editor/test/inputEditor_test.js
@@ -3,6 +3,7 @@ const { JSDOM } = require('jsdom');
 
 const createApplication = require('./createApplication');
 const EditorPage = require('./editor.page');
+const { capturePage, reportLog } = require('./helper.js');
 
 describe('에디터 입력 테스트', function() {
   this.timeout(10000);
@@ -13,7 +14,13 @@ describe('에디터 입력 테스트', function() {
     return app.start();
   });
 
-  afterEach(() => {
+  afterEach(function () {
+    if (this.currentTest.state === "failed") {
+      return Promise.all([
+        capturePage(app, this.currentTest.title),
+        reportLog(app, this.currentTest.title)
+      ]).then(() => app.stop());
+    }
     return app.stop();
   });
 
@@ -33,6 +40,15 @@ describe('에디터 입력 테스트', function() {
           const h2 = document.querySelector('h2');
           assert.equal(h2.textContent, 'h2 제목');
         });
+    });
+  });
+
+  describe('이모티콘 입력하기', function() {
+    it('이모티콘 PNG 이미지가 렌더링되었습니다.', function() {
+      const page = new EditorPage(app.client);
+      return page.inputText(':tada:')
+        .then(() => page.findEmojiElement('tada'))
+        .then((element) => assert(!!element));
     });
   });
 });
